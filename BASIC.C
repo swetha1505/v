4a20a27b128b3d136d7d622e9d2b8fdd69961d3e
swetha1505 <93249383+swetha1505@users.noreply.github.com>
2021-10-27 15:09:53 +0530

    Add files via upload

---
+++ BASIC.C
@@ -0,0 +1,13 @@
+#include<stdio.h>
+//variable here
+int swetha=0;
+
+*******************************main******************************
+void main()
+{
+clrscr();
+	printf("hello world");
+	printf("%d",swetha);
+getch();
+}
+******************************end*******************************
